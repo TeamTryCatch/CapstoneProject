6be1d0a031f1cabf6b706d412b492bb51b14271f
Sarthak <sarthak.sharma02@gmail.com>
2018-09-25 23:06:47 -0400

    VariousWorkings of the Grid

--- my-app/src/SummaryPage/SummaryPage.js
+++ my-app/src/SummaryPage/SummaryPage.js
@@ -5,19 +5,25 @@ import "ag-grid-enterprise";
 // import "ag-grid-enterprise";
 import UniversityData from '../Data/UniversityData.json';
 import 'ag-grid-community/dist/styles/ag-grid.css';
-import 'ag-grid-community/dist/styles/ag-theme-balham.css';
+import 'ag-grid-community/dist/styles/ag-theme-material.css';
 
 class SummaryPage extends Component {
     constructor(props) {
         super(props);
+        
+        let imageArray = [
+            '../Data/algoma.jpg',
+            '../Data/gh.png',
+            '../Data/guelph.png'
+        ];
 
         this.state = {
             columnDefs: [
-                {headerName: "School Name", field: "School Name", cellRenderer: "agGroupCellRenderer", rowGroup: true, hide: true, pinned: "left", filter: "agTextColumnFilter"},
+                {headerName: "School Name", field: "School Name", cellRenderer: this.customCellRendererFunc, rowGroup: true, hide: false, filter: "agTextColumnFilter"},
                 {headerName: "OUAC Code", field: "OUAC Code", filter: "agTextColumnFilter"},
                 {headerName: "Program Name", field: "Program Name", filter: "agTextColumnFilter"},
                 {headerName: "Ontario Secondary School Prerequisites", field: "Ontario Secondary School Prerequisites", filter: "agTextColumnFilter"},
-                {headerName: "Alternative Offer(s)", field: "Alternative Offer(s)", filter: "agTextColumnFilter"},
+                {headerName: "Co-op Option", field: "Co-op Option", filter: "agTextColumnFilter"},
             ],
         groupDefaultExpanded: 1,
         rowData: UniversityData
@@ -27,17 +33,21 @@ class SummaryPage extends Component {
     render() {
         return (
                 <div 
-                  className="ag-theme-balham"
+                  className="ag-theme-material"
                   style={{ 
 	                height: '1000px', 
-	                width: '2000px' }} 
-		            >
+                    width: '2000px' 
+                }} 
+                    >
                     <AgGridReact
                     columnDefs={this.state.columnDefs}
                     animateRows={true}
                     enableRangeSelection={true}
+                    enableColResize={true} 
+                    suppressSizeToFit={true}
                     enableSorting={true}
                     enableFilter={true}
+                    floatingFilter={true}
                     rowData={this.state.rowData}>
                     </AgGridReact>
                 </div>
