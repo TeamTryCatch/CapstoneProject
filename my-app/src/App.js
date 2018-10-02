a88a2313111302b05882645c4dc8cd791c590438
Sarthak <sarthak.sharma02@gmail.com>
2018-10-01 22:54:33 -0400

    graphing

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,12 +2,17 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import SideBar from './SideBar/SideBar';
-
+import Analytics from './Analytics/Analytics.js';
 class App extends Component {
+  
   render() {
     return (
       <div className="App">
-        <SideBar />
+      <Analytics />
+      <Analytics />
+      <Analytics />
+      <Analytics />
+      <SideBar />        
       </div>
     );
   }
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-
+import Analytics from './Analytics/Analytics.js';
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
